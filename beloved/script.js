4cb4eb893c6bfaef2af8fb7da0a97d270ac9003b
revisetiawanrs <163722220+revisetiawanrs@users.noreply.github.com>
2024-03-17 16:08:37 +0700

    Add files via upload

---
+++ beloved/script.js
@@ -0,0 +1,10 @@
+function myfunction() {
+    document.getElementById("button").style.display = "none"
+    document.getElementById("gambar").src = "—Pngtree—happy birthday font text balloon_8186777.png";
+    document.getElementById("tulisan").innerHTML = "Selamat ulang tahun adek manis, semoga panjang umur, sehat selalu, dan semoga apa yang diingkan tercapai. Jangan nyebelin ya tetep jadi bocil wkwk, dah pokoke yang baik-baik aja aku bantu aamiinin. gabisa merangkai kata-kata, intinya selamat ulang tahun ya, love."
+    document.getElementById("musik").style.display = "none"
+    document.getElementById("text1").
+      innerHTML = "❤️❤️❤️❤️"
+    document.getElementById("text2").style.display = "none"
+    document.getElementById("text").innerHTML = "LOVE YOU"
+  }
